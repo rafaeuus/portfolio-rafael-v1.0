286380ac91ecd4c2a330896262a0e98c1fb1c948
rafaeuus <rafaeljcarvalhosilva@gmail.com>
2023-07-05 13:57:31 -0300

    first commit

---
+++ src/utils/userData.ts
@@ -0,0 +1,9 @@
+export const userData = {
+  nameUser: "Rafael Carvalho",
+  githubUser: "rafaeuus",
+  linkedinUser: "rafael-s-carvalho",
+  whatsappNumber: "+5511966029526",
+  emailUser: "rafaeljcarvalhosilva@gmail.com",
+  instagramUser: "rafael_s.carvalho",
+  facebookUser: "",
+};
